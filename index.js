3c76a5accb92e1136467205a644a1bdf76d0ce69
Shohidul Arifin <shohidularifin@gmail.com>
2023-03-02 00:49:32 -0800

    mongoose model added and saving model

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ mongoose.connect('mongodb://localhost:27017/my-student')
 .then(()=> console.log('mongodb connected successfully'))
 .catch(err=> console.log('connection failed'));
 
-
+//  Schema 
 // use new key word when create object from a class
 const studentSchema = new mongoose.Schema({
     firstName: {type: String},
     lastName: String,
     dob:Date,
-    entryDate: { type: DataTransfer, default:Date.now},
+    entryDate: { type: Date, default:Date.now},
     passed: Boolean,
     hobbies:[String],
     parents:{
@@ -21,4 +21,22 @@ const studentSchema = new mongoose.Schema({
 
 });
 
+// Model
 
+const Student = mongoose.model('Student',studentSchema); //Class
+
+const student = new Student({
+    firstName: "Rahim",
+    lastName: "Ahmed",
+    dob: new Date("30 december 1987"),
+    passed: true,
+    hobbies:["Swiming", "Singing"],
+    parents:{
+        father: "Abdul",
+        mother: "Rahima"
+    },
+    subjects:[{name:"English", marks:80 }, {name: "Math", mark:90}]
+})
+
+student.save()
+.then(data => console.log(data))
